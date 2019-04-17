e8a7c3ab855102bbe2f66fe7f21b219ca3dcceb5
Mila Ivanova <mila.ivanova97@gmail.com>
2019-04-17 17:40:47 +0300

    First commit

---
+++ CurrencyConverter/Calculations.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+   public class Calculations
+    {
+        public const double usd = 1.72761;
+        public const double eur = 1.95583;
+        public const double bgn = 1.0;
+        public const double gbp = 2.26658;
+
+        public static double CHANGE;
+
+        public static double getCurrency(double amount, string currIn, string currOut)
+        {
+            double currencyIn = 0.0;
+            double currencyOut = 0.0;
+            double result = 0.0;
+
+            switch (currIn)
+            {
+                case "USD":
+                    currencyIn = usd;
+                    break;
+                case "EUR":
+                    currencyIn = eur;
+                    break;
+                case "BGN":
+                    currencyIn = bgn;
+                    break;
+                case "GBP":
+                    currencyIn = gbp;
+                    break;
+                default:
+                    break;
+            }
+            switch (currOut)
+            {
+                case "USD":
+                    currencyOut = usd;
+                    break;
+                case "EUR":
+                    currencyOut = eur;
+                    break;
+                case "BGN":
+                    currencyOut = bgn;
+                    break;
+                case "GBP":
+                    currencyOut = gbp;
+                    break;
+                default:
+                    break;
+            }
+            result = amount*(currencyIn/currencyOut);
+            CHANGE = getChange(amount, result, currencyIn, currencyOut);
+            return result;
+        }
+
+        public static double getChange(double amount,double result, double currencyin, double currencyout)
+        {
+            double change = amount - (Math.Truncate(result) / (currencyin / currencyout));
+            return change;
+        }
+    }
+}
---
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FillComboBoxes();
+        }
+
+       private void FillComboBoxes()
+        {
+            CmBoxIn.Items.Add("USD");
+            CmBoxIn.Items.Add("EUR");
+            CmBoxIn.Items.Add("BGN");
+            CmBoxIn.Items.Add("GBP");
+
+            CmBoxOut.Items.Add("USD");
+            CmBoxOut.Items.Add("EUR");
+            CmBoxOut.Items.Add("BGN");
+            CmBoxOut.Items.Add("GBP");
+        }
+
+        private void getResults(double amount)
+        {
+            txtOutput.Text = Math.Truncate(amount).ToString();
+            txtChange.Text = String.Format("{0:0.00}", Calculations.CHANGE);
+        }
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double isDouble = 0.0;
+
+            if ((double.TryParse(txtInput.Text.ToString(), out isDouble)) == true)
+            {
+                if (CmBoxIn.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please select a currency to convert!");
+                }
+                else if (CmBoxOut.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please select a converted currency!");
+                }
+                else
+                {
+                    getResults(Calculations.getCurrency(Double.Parse(txtInput.Text), CmBoxIn.Text.ToString(), CmBoxOut.Text.ToString()));
+                }
+            }
+        }
+
+        private void CmBoxIn_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtInput.Clear();
+            txtOutput.Clear();
+            txtChange.Clear();
+
+        }
+    }
+}
